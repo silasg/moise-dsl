c8d478b08b4c5a88ce553d04310c73790b5a8080
silas <silas@b53e0bb7-f178-486d-8d5c-d48413fdacd7>
2011-05-14 13:56:45 +0000

    import

---
+++ src/moise/dsl/fs/TimeTerm.scala
@@ -0,0 +1,45 @@
+package moise.dsl.fs
+
+trait TimeTerm {
+  //TODO: noch package-private machen, hat von außen nix zu tun
+  def toAttributeString: String
+}
+
+case class TimeSpan(duration: Int) extends TimeTerm {
+  implicit def intToTimeSpan(i: Int) = TimeSpan(i)
+
+  def milliseconds = duration
+  def seconds = milliseconds * 1000
+  def minutes = seconds * 60
+  def hours = minutes * 60
+  def days = hours * 24
+  def years = days * 365
+
+  def millisecond = milliseconds
+  def second = seconds
+  def minute = minutes
+  def hour = hours
+  def day = days
+  def year = years
+
+  def and (ts: TimeSpan) = TimeSpan(milliseconds + ts.milliseconds)
+  def +(ts: TimeSpan) = and(ts)
+
+  def toAttributeString = {
+    if (milliseconds % 1.year == 0) milliseconds / 1.year + " years"
+    else if (milliseconds % 1.day == 0) milliseconds / 1.day + " days"
+    else if (milliseconds % 1.hour == 0) milliseconds / 1.hour + " hours"
+    else if (milliseconds % 1.minute == 0) milliseconds / 1.minute + " minutes"
+    else if (milliseconds % 1.second == 0) milliseconds / 1.second + " seconds"
+    else milliseconds + " milliseconds"
+  }
+}
+
+object Now extends TimeTerm {
+  def toAttributeString = "now"
+}
+
+object Never extends TimeTerm {
+  def toAttributeString = "never"
+}
+
