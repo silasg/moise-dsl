f126953870071eff2235bea9149012727d495428
silas <silas@b53e0bb7-f178-486d-8d5c-d48413fdacd7>
2011-06-05 14:57:42 +0000

    Interpreter

---
+++ src/moise/dslinterpreter/MoiseInterpreter.scala
@@ -0,0 +1,32 @@
+package moise.dslinterpreter
+
+import scala.tools.nsc._
+import scala.io.Source
+
+import moise.dsl.os.OS
+import moise.dslconverter.XMLConverter
+ 
+object MoiseInterpreter {
+  def interpret (dslFile: String): String = {
+    var settings = new Settings
+    settings.usejavacp.value = true
+    var res = Array[AnyRef](null)
+    val i = new Interpreter(settings)
+    i.bind("result", "Array[AnyRef]", res)
+    i.interpret("import moise.dsl.fs._")
+    i.interpret("import moise.dsl.ss._")
+    i.interpret("import moise.dsl.ns._")
+    i.interpret("import moise.dsl.os._")
+    i.interpret("import moise.dsl.helper.Implicits._")
+    Source.fromFile(dslFile).getLines.foreach(i interpret _)
+    i.interpret("result(0) = res0")
+    val os = res(0).asInstanceOf[OS]
+    val osXml = XMLConverter toXml os
+    osXml.toString
+  }
+
+  /**
+   * For testing/debugging only since I don't want an unit test with reference to an external file
+   */
+  def main (args: Array[String]) = println(interpret(args(0)))
+}
